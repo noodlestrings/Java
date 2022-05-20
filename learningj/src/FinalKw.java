f11d4181028eaccac5aadf634aa3443eaac18715
Sam Lee <sammystradling2006@gmail.com>
2022-05-20 21:08:18 +0100

    FinalKw.java

---
+++ learningj/src/FinalKw.java
@@ -0,0 +1,8 @@
+public class FinalKw {
+    public static void main(String[] args){
+        // finals cannot be changed/update later in the program
+        final double PI = 3.141592654; // sort of like a constant
+        //PI = 3;
+        System.out.println(PI);
+    }
+}
