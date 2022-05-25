e10431c209fa11f0a71b077555a2faee61bce2f7
Sam Sidcup <sammystradling2006@gmail.com>
2022-05-25 15:48:34 +0100

    test

--- PasswordGenerator/src/Main.java
+++ PasswordGenerator/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //testcommit
         StringBuilder password = new StringBuilder();
         Random selectArray = new Random();
         Random selectValue = new Random();
