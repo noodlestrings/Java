92431113b1fed8461abba95398993700cabad10c
Sam Lee <sammystradling2006@gmail.com>
2022-07-04 14:09:34 +0100

    Got reading + writing to users.txt working

--- Bank/src/Main.java
+++ Bank/src/Main.java
@@ -1,5 +1,36 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        Read("C:\\IntellijProjects\\Bank\\src\\users.txt");
+
+        try {
+            FileWriter out = new FileWriter("C:\\IntellijProjects\\Bank\\src\\users.txt", true);
+            out.write("This is a test from FileWriter");
+            out.close();
+
+            System.out.println("\nLine successfully written\n");
+
+
+            Read("C:\\IntellijProjects\\Bank\\src\\users.txt");
+
+
+        } catch (IOException e){ //any exception
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+    }
+    static void Read(String fileName) throws Exception { // static means no object needs to be instantiated
+        File file = new File(fileName);                  // to access this method
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
     }
 }
