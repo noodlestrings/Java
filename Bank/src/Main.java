fdabb5fbc57616ef61a38fb798072ebaad3c535f
Sam Lee <sammystradling2006@gmail.com>
2022-07-04 14:16:12 +0100

    refactored + optimised; added read.java containing read method

--- Bank/src/Main.java
+++ Bank/src/Main.java
@@ -1,22 +1,21 @@
-import java.io.File;
 import java.io.FileWriter;
-import java.util.Scanner;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String fileName = "C:\\IntellijProjects\\Bank\\src\\users.txt";
 
-        Read("C:\\IntellijProjects\\Bank\\src\\users.txt");
+        Read.read();
 
         try {
-            FileWriter out = new FileWriter("C:\\IntellijProjects\\Bank\\src\\users.txt", true);
-            out.write("This is a test from FileWriter");
+            FileWriter out = new FileWriter(fileName, true);
+            out.write("This is a test from FileWriter\n");
             out.close();
 
             System.out.println("\nLine successfully written\n");
 
 
-            Read("C:\\IntellijProjects\\Bank\\src\\users.txt");
+            Read.read();
 
 
         } catch (IOException e){ //any exception
@@ -24,7 +23,9 @@ public class Main {
             e.printStackTrace();
         }
     }
-    static void Read(String fileName) throws Exception { // static means no object needs to be instantiated
+
+
+    /*static void Read(String fileName) throws Exception { // static means no object needs to be instantiated
         File file = new File(fileName);                  // to access this method
         Scanner sc = new Scanner(file);
 
@@ -32,5 +33,5 @@ public class Main {
             System.out.println(sc.nextLine());
         }
         sc.close();
-    }
+    }*/
 }
---
+++ Bank/src/Read.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Read {
+    static void read() throws Exception { // static means no object needs to be instantiated
+        File file = new File("C:\\IntellijProjects\\Bank\\src\\users.txt");                  // to access this method
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()){
+            System.out.println(sc.nextLine());
+        }
+        sc.close();
+    }
+}
