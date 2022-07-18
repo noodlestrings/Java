4e4fcd2037e3d215d16ecd17185cef3ae69266aa
Macbook air <sammystradling2006@gmail.com>
2022-07-19 00:25:32 +0100

    test

--- PasswordGenerator/src/Main.java
+++ PasswordGenerator/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //testcommit
+        //testcommit1
         StringBuilder password = new StringBuilder();
         Random selectArray = new Random();
         Random selectValue = new Random();
